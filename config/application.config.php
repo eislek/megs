d0460b9b3f5d193b25a45452f625497925e3bd8c
mgueney <meryemgueney@hotmail.de>
2016-06-05 18:15:02 +0200

    Login, Passwort vergessen, Registrieren

--- config/application.config.php
+++ config/application.config.php
@@ -9,10 +9,13 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
-        'Erinnerung',
-        'Benutzer',
-        'Log',
-        'Blacklist'
+        'DoctrineModule',
+        'DoctrineORMModule',
+        'CsnUser',
+        //'Erinnerung',
+        //'Benutzer',
+        //'Log',
+        //'Blacklist'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
